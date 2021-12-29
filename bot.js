3421377986061dd27b74d30fa4b4bc9ac68ac777
Aidan Makare <aidanmakaree@gmail.com>
2021-12-28 20:21:43 -0600

    Replaced word API with npm random-words method
    - Shortened the code significantly
    
    - Added checks: if DB does not contain random word
    
    Note: working towards looping the switch case in the case that the word generated is not within the rhyme API

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.io");
 const auth = require("./auth.json");
 const axios = require("axios");
 const fetch = require("node-fetch");
+var randomWords = require("random-words");
 
 const bot = new Discord.Client({
   token: auth.token,
@@ -20,6 +21,9 @@ bot.on("message", function (user, userID, channelID, message, evt) {
     var cmd = args[0];
     args = args.splice(1);
 
+    var word = [];
+    var data = [];
+
     switch (cmd) {
       case "ping":
         bot.sendMessage({
@@ -30,38 +34,43 @@ bot.on("message", function (user, userID, channelID, message, evt) {
 
       case "testAPI":
         //Generate random word variable for rhyming API and sendMessage
-        fetch("https://random-words-api.vercel.app/word")
-          .then((res) => res.json())
-          .then((json) => {
-            let someJson = JSON.stringify(json)
-              .split(":")[1]
-              .split(",")[0]
-              .replace(/["]+/g, "");
-            console.log(someJson);
-            bot.sendMessage({
-              to: channelID,
-              message: `What rhymes with the word: ${someJson}`,
-            });
-            //Uses random word variable to get rhyming words from API
-            fetch(`https://api.datamuse.com/words?rel_rhy=${someJson}`)
-              .then((res) => res.json())
-              .then((json) => {
-                let rhymeJson = JSON.stringify(json);
-                console.log(rhymeJson);
-                bot.sendMessage({
-                  to: channelID,
-                  message: `${rhymeJson}`,
-                });
-                rhymeJson = JSON.parse(rhymeJson)
-                var data = [];
+        // fetch("https://random-words-api.vercel.app/word")
+        //   .then((res) => res.json())
+        //   .then((json) => {
+        //     let someJson = JSON.stringify(json)
+        //       .split(":")[1]
+        //       .split(",")[0]
+        //       .replace(/["]+/g, "");
+        //     console.log(someJson);
+        var flag = true;
+        while (flag == true) {
+          word = randomWords(1);
+          //Uses random word variable to get rhyming words from API
+          fetch(`https://api.datamuse.com/words?rel_rhy=${word}`)
+            .then((res) => res.json())
+            .then((json) => {
+              let rhymeJson = JSON.stringify(json);
+              if (rhymeJson.charAt(1) != "]") {
+                rhymeJson = JSON.parse(rhymeJson);
+
                 for (let i = 0; i < rhymeJson.length; i++) {
                   data.push(rhymeJson[i].word);
-                  console.log(data[i]);
+                  
                 }
-              });
-          });
+                console.log(data[0]);
 
-        break;
+                bot.sendMessage({
+                  to: channelID,
+                  message: `What rhymes with the word: ${word}`,
+                });
+                flag = false;
+              } else {
+                console.error("Error - API gave word with no rhyme DB");
+              }
+            });
+
+          break;
+        }
     }
   }
 });
