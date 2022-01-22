e38270cd296eff1503c84f9d73d94c4d414cc91b
Aidan Makare <aidanmakaree@gmail.com>
2022-01-21 18:04:45 -0600

    Attempting to create game class
    Creating class logic

--- bot.js
+++ bot.js
@@ -3,14 +3,7 @@ const axios = require("axios");
 const fetch = require("node-fetch");
 var randomWords = require("random-words");
 require("dotenv").config();
-const { fillJsonArray, initialMessage, followUpMessage, finalMessage } = require("./functions");
-
-var rData = [];
-var word = [];
-var check;
-var counter;
-var flag = true;
-var maxCounter = 10;
+var Game = require('./game');
 
 //Creating bot with token
 var bot = new Discord.Client({
@@ -43,10 +36,7 @@ bot.on("message", (user, userID, channelID, message, evt) => {
     }
 
     if (cmd === "reset") {
-      rData = [];
-      flag = true;
-      check = undefined;
-      maxCounter = 10;
+      delete newGame;
       bot.sendMessage({
         to: channelID,
         message:
@@ -63,46 +53,48 @@ bot.on("message", (user, userID, channelID, message, evt) => {
        */
       //Generates a random word and uses that word to retrieve the rhyming JSON from the datamuse API
       //It then fills an array, rData, with all the words in the JSON that rhyme with the random word generated
-      if (flag) {
-        word = randomWords(1);
-        flag = false;
+
+      var newGame = new Game();
+      if (newGame.flag) {
+        newGame.word = randomWords(1);
+        newGame.flag = false;
       }
-      fetch(`https://api.datamuse.com/words?rel_rhy=${word}`)
+      fetch(`https://api.datamuse.com/words?rel_rhy=${newGame.word}`)
         .then((res) => res.json())
         .then((json) => {
           let rhymeJson = JSON.stringify(json);
           if (rhymeJson.charAt(1) != "]") {
             rhymeJson = JSON.parse(rhymeJson);
-            check = fillJsonArray(rhymeJson, rData); //Helper function defined below
-            counter = rData.length;
-            if (counter >= 10) {
-              initialMessage(channelID, word, counter, check, maxCounter,bot); //Helper function defined below
+            newGame.check = newGame.fillJsonArray(rhymeJson, newGame.rData); //Helper function defined below
+            newGame.counter = newGame.rData.length;
+            if (newGame.counter >= 10) {
+              newGame.initialMessage(channelID, newGame.word, newGame.counter, newGame.check, newGame.maxCounter,bot); //Helper function defined below
               //Bot listens to messages in channel and announces if a word has been said that rhymes with the generated word
               bot.on(
                 "message",
                 (user, userID, channelID, message2, rawEvent) => {
-                  if (maxCounter != 0) {
-                    for (let j = 1; j < rData.length; j++) {
+                  if (newGame.maxCounter != 0) {
+                    for (let j = 1; j < newGame.rData.length; j++) {
                       //console.log(rData[j]);
                       if (message2 != "^rhyme") {
-                        if (message2 === rData[j]) {
-                          rData = rData.filter((e) => e !== message2);
-                          counter--;
-                          maxCounter--;
-                          if (maxCounter == 0) {
-                            finalMessage(channelID, message2, word, bot); //Helper function defined below
-                            rData = [];
-                            flag = true;
-                            check = undefined;
-                            maxCounter = 10;
+                        if (message2 === newGame.rData[j]) {
+                          newGame.rData = newGame.rData.filter((e) => e !== message2);
+                          newGame.counter--;
+                          newGame.maxCounter--;
+                          if (newGame.maxCounter == 0) {
+                            newGame.finalMessage(channelID, message2, newGame.word, bot); //Helper function defined below
+                            newGame.rData = [];
+                            newGame.flag = true;
+                            newGame.check = undefined;
+                            newGame.maxCounter = 10;
                             return;
                           }
-                          followUpMessage(
+                          newGame.followUpMessage(
                             channelID,
                             message2,
-                            word,
-                            maxCounter,
-                            counter,
+                            newGame.word,
+                            newGame.maxCounter,
+                            newGame.counter,
                             bot
                           ); //Helper function defined below
                         }
@@ -113,14 +105,14 @@ bot.on("message", (user, userID, channelID, message, evt) => {
               );
             } else {
               console.error("Counter error");
-              rData = [];
-              flag = true;
-              check = undefined;
-              maxCounter = 10;
+              newGame.rData = [];
+              newGame.flag = true;
+              newGame.check = undefined;
+              newGame.maxCounter = 10;
             }
           } else {
             console.error("Invalid word error");
-            flag = true;
+            newGame.flag = true;
           }
         });
     }
